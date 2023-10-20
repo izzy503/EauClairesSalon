9657af565d4e01bec69e167fb1cf916fa69c6c9a
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-10-19 17:58:19 -0700

    re-adding files and folders

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EauClaireSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public int StylistId { get; set; }
+
+    public Stylist Stylist { get; set; }
+  }
+}
