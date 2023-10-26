02099a4f4aec1c35bc02a7bff34e42d95642cdd7
ipc <israelpadilla@Israels-MacBook-Air.local>
2023-10-25 20:00:38 -0700

    added new sql dump

--- Izzy_Padilla.sql
+++ Izzy_Padilla.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.1.0, for macos13.3 (arm64)
 --
--- Host: localhost    Database: HairSalon
+-- Host: localhost    Database: Hairsalon
 -- ------------------------------------------------------
 -- Server version	8.0.27
 
@@ -22,21 +22,21 @@
 DROP TABLE IF EXISTS `Clients`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Client` (
+CREATE TABLE `Clients` (
   `ClientId` int unsigned NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) DEFAULT NULL,
-  `StylistId` int unsigned NOT NULL,
-  `Stylist` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ClientId`,`StylistId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `StylistId` int DEFAULT NULL,
+  PRIMARY KEY (`ClientId`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Client`
+-- Dumping data for table `Clients`
 --
 
 LOCK TABLES `Clients` WRITE;
 /*!40000 ALTER TABLE `Clients` DISABLE KEYS */;
+INSERT INTO `Clients` VALUES (1,'Boolean',0),(2,'Boolean',0),(3,'Dave',0),(4,'hhhh',0),(5,'Boolean',0),(6,'Boolean',0),(7,'Chandler Scott',0),(8,'voofal',0),(9,'Boolean',0),(10,'Chandler Scott',0),(11,'Chandler Scott',0),(12,'d',0),(13,'d',0),(14,'Boolean',0),(15,'add',0),(16,'gab',0),(17,'voofal',0),(18,'Tarka',0),(19,'dawn',0),(20,'Boolean',0),(21,'Tarka',1),(22,'dean',3),(23,'Ryan',5),(24,'Ashley',5);
 /*!40000 ALTER TABLE `Clients` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +52,7 @@ CREATE TABLE `Stylists` (
   `Name` varchar(255) DEFAULT NULL,
   `Specialty` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`StylistId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,6 +61,7 @@ CREATE TABLE `Stylists` (
 
 LOCK TABLES `Stylists` WRITE;
 /*!40000 ALTER TABLE `Stylists` DISABLE KEYS */;
+INSERT INTO `Stylists` VALUES (1,'gabe','fade'),(2,'blag','hhh'),(3,'Boolean','Hand Surgeon'),(4,'geno','hands'),(5,'Ryan','hair');
 /*!40000 ALTER TABLE `Stylists` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -73,4 +74,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-19 13:40:05
+-- Dump completed on 2023-10-25 19:57:35
